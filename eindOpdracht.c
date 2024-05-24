adae64baaa35babfb7580e4374a4fd4f3fde2ab6
Laye19 <Laye.kone@student.pxl.be>
2024-05-25 01:26:04 +0200

    "Add the Custum Different structure"

--- eindOpdracht.c
+++ eindOpdracht.c
@@ -13,27 +13,56 @@ struct KeyInfo {
     char* keyValue;
 };
 
-// Enumerated components
-enum GeometryComponents { VOLUME, SURFACE, MASS };
+enum GeometryComponents { VOLUME, SURFACE, MASS };// Enumerated components
 enum ShapeType { SPHERE, CONE, CYLINDER, LINE, CUBE }; // Add more shapes if needed
 
 // Custom strsep function
 char *myCustomStrsep(char **stringPtr, const char *delimiter) {
     char *result = *stringPtr;
     if (result) {
-        *stringPtr += strcspn(*stringPtr, delimiter);
+        *stringPtr += strcspn(*stringPtr, delimiter); // Skip leading delimiters if any 
         if (**stringPtr)
             *(*stringPtr)++ = '\0';
         else
-            *stringPtr = NULL;
-    }
+            *stringPtr = NULL;}
     return result;
 }
+
+// Custom structure for shape informat
+ struct shape{ 
+    enum ShapeType shapeType; // Shape type (SPHERE, CONE, CYLINDER, LINE, CUBE)
+    int size;  // Size of the shape (e.g. radius, height, length, etc.) 
+ };
+
+struct AttackType{ // Custom structure for attack type information 
+    char *name; // Name of the attack type 
+    char *index; // Index of the attack type     
+    char *url; // URL of the attack type 
+};
+
+// Custom structure for a list of attack types
+enum AttackTypeList{attack_at_characterelevel, attack_at_slot_level}; // Enumerated attack types 
+ struct attackList{ 
+    enum AttackTypeList AttackTypeList; // Attack type information 
+    struct attackList *values; // Pointer to the next attack type 
+ };
+
+ struct response{ // Custom structure for response information 
+    char *name; // Name of the response 
+    char *index; // Index of the response 
+    char *url; // URL of the response
+};
+
+struct responseList{ // Custom structure for a list of responses 
+    struct response *response_type; // Response information 
+    char *values; // Pointer to the next response 
+};
 
 // Define the maximum list size
 #define MAX_LIST_SIZE (100)
 
-int main() {
+int main(int argc, char *argv[]) 
+{
     // Your code logic here
     // ...
     return 0;
